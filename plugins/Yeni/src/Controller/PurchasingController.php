6509af7520e74ad82e310af1b42822b21205f344
Hungbg <bubugihu@gmail.com>
2024-03-07 14:22:42 +0700

    fix bug trim()

--- plugins/Yeni/src/Controller/PurchasingController.php
+++ plugins/Yeni/src/Controller/PurchasingController.php
@@ -51,7 +51,7 @@ class PurchasingController extends AppController
 
                         foreach ($dataInput as $key => $value)
                         {
-                            if($key <= 1 || empty(trim($value['B'])))
+                            if($key <= 1 || empty($value['B']) || trim($value['B']) == "" )
                             {
                                 continue;
                             }
